6ebfee1ba6d839a6b4fefef8b27447f9a1b9d573
sergei-mikhailovskii-idf <uSQcBQpfxPqTLDiwutWvSX>
2021-03-31 14:13:59 +0300

    Added retrieving all contacts

--- app/src/main/java/com/example/contactstest/MainActivity.kt
+++ app/src/main/java/com/example/contactstest/MainActivity.kt
@@ -7,18 +7,26 @@ import android.util.Log
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import android.provider.ContactsContract.CommonDataKinds.Phone
 
 
 class MainActivity : AppCompatActivity() {
 
-    private val contactsPermissionRequest =
+    private val contactPermissionRequest =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) {
             if (it) {
-                getContactsRequest.launch(null)
+                getContactRequest.launch(null)
             }
         }
 
-    private val getContactsRequest =
+    private val contactsPermissionRequest =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (it) {
+                getContacts()
+            }
+        }
+
+    private val getContactRequest =
         registerForActivityResult(ActivityResultContracts.PickContact()) {
             val contentCursor = contentResolver.query(it, null, null, null, null)
                 ?: return@registerForActivityResult
@@ -30,15 +38,15 @@ class MainActivity : AppCompatActivity() {
                     contentCursor.getString(contentCursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))
                 if (hasPhone.equals("1", ignoreCase = true)) {
                     val phones = contentResolver.query(
-                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        Phone.CONTENT_URI,
                         null,
-                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + id,
+                        Phone.CONTACT_ID + " = " + id,
                         null,
                         null
                     ) ?: return@registerForActivityResult
                     phones.moveToFirst()
                     val contactNumber: String =
-                        phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                        phones.getString(phones.getColumnIndex(Phone.NUMBER))
                     Log.d("contact", "The phone number is $contactNumber")
                     phones.close()
                 }
@@ -46,12 +54,41 @@ class MainActivity : AppCompatActivity() {
             contentCursor.close()
         }
 
+    private fun getContacts() {
+        val managedCursor = contentResolver.query(
+            Phone.CONTENT_URI,
+            arrayOf(Phone._ID, Phone.DISPLAY_NAME, Phone.NUMBER),
+            null,
+            null,
+            Phone.DISPLAY_NAME + " ASC"
+        )
+
+        if (managedCursor?.moveToFirst() == true) {
+            Log.d(MainActivity::class.java.simpleName, "Size: ${managedCursor.count}")
+            while (!managedCursor.isAfterLast) {
+                Log.d(
+                    MainActivity::class.java.simpleName,
+                    "Phone: ID: ${Phone._ID}, DISPLAY_NAME: ${Phone.DISPLAY_NAME}, NUMBER: ${Phone.NUMBER}"
+                )
+                managedCursor.moveToNext()
+            }
+        }
+
+        managedCursor?.close()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val text = findViewById<TextView>(R.id.text)
-        text.setOnClickListener {
+        val tvRetrievePhoneNumber = findViewById<TextView>(R.id.tv_retrieve_phone_number)
+        val tvRetrieveContactsList = findViewById<TextView>(R.id.tv_retrieve_contacts_list)
+
+        tvRetrievePhoneNumber.setOnClickListener {
+            contactPermissionRequest.launch(Manifest.permission.READ_CONTACTS)
+        }
+
+        tvRetrieveContactsList.setOnClickListener {
             contactsPermissionRequest.launch(Manifest.permission.READ_CONTACTS)
         }
 
